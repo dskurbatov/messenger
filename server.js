01e443811d2753afdab3a43a8002c45404eacfb9
Dima Kurbatov <dskurbatov13@gmail.com>
2017-12-08 19:52:54 -0800

    add support for static files

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const Koa = require('koa')
+const serve = require('koa-static')
 
 const app = new Koa()
 
-app.use(async ctx => ctx.body = 'Hello World')
+app.use(serve('public'))
 
-app.listen(3000, () => {
+app.listen(3000, async () => {
 	console.log('Server listens on ports 3000')
 })
